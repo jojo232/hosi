486a8bf2058cef362c08845c782f21aa9dbc007b
Ubuntu <ubuntu@ip-172-31-12-186.ap-northeast-1.compute.internal>
2021-04-27 09:50:50 +0000

    yuya 16

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,20 +1,12 @@
-// class = "ratings"の子要素を取得
+
 const stars = document.querySelector(".ratings").children;
-// const stars = document.getElementsByClassName("ratings").children;
-// id = "rating-value"の要素を取得
 const ratingValue = document.getElementById("rating-value");
-// id = "rating-value-display"の要素を取得
 const ratingValueDisplay = document.getElementById("rating-value-display");
-// indexという変数を定義
 let index;
 
-// reveiwの投稿ページに遷移するとstars.lengthの値（5）だけ繰り返し
 for(let i=0; i<stars.length; i++){
 	console.log(stars.length)
-	// 星にカーソルが乗ったときに実行する関数を定義
 	stars[i].addEventListener("mouseover",function(){
-		// stars.lengthの値（5）だけ繰り返し
-		// つまり、星カーソルが乗った時、5回繰り返される
 		for(let j=0; j<stars.length; j++){
 			console.log(stars.length)
 			// まず全て星をくり抜く
